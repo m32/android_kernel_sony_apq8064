d82948c0b82ac054ebd424a4319c888363f88bae
Chippa-a <vusal1372@gmail.com>
2016-12-29 15:38:15 +0300

    fs: Update f2fs shrinkers to new API
     * From linux-4.4.y branch
    
    Change-Id: Ib926035c56adc2143a51fc6bbe2717432a42ae63

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -66,7 +66,8 @@ static void f2fs_build_fault_attr(unsigned int rate)
 
 /* f2fs-wide shrinker description */
 static struct shrinker f2fs_shrinker_info = {
-	.shrink = f2fs_shrink_scan,
+	.scan_objects = f2fs_shrink_scan,
+	.count_objects = f2fs_shrink_count,
 	.seeks = DEFAULT_SEEKS,
 };
 
